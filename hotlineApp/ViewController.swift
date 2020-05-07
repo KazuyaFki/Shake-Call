75f83b6bdffcdd8862ae3b4429f27ad23917a54b
KazuyaFki <complete501xx@yahoo.co.jp>
2020-05-07 23:29:47 +0900

    Initial Commit

---
+++ hotlineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hotlineApp
+//
+//  Created by Kazuya Fukui on 2020/05/07.
+//  Copyright © 2020 Kazuya Fukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
